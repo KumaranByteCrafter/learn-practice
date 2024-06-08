0340544ff80e58b59df7835f4b91e72d1232a80f
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-06-08 18:08:03 +0000

    learn about varianle in c

---
+++ var.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    //- single line command
+    //declaration of the variable using datatypes
+    int a = 10,b,c=30,d;
+    float e=1.23,f,g,h=10;
+    char i = 'a',j='b';
+    b = a + c;
+    f = e+h+20+30.21;
+    printf("value of b is %d\n",b);//%d for integer
+    printf("value of f is %f\n",f);// %f for float 
+}
