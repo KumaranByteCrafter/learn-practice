3cac4ad213720b3762a0a999579c232d1e41bb27
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-06-08 18:10:24 +0000

    learn about char

--- var.c
+++ var.c
@@ -4,9 +4,11 @@ int main(){
     //declaration of the variable using datatypes
     int a = 10,b,c=30,d;
     float e=1.23,f,g,h=10;
-    char i = 'a',j='b';
+    char i = 'a',j='b'; 
     b = a + c;
     f = e+h+20+30.21;
     printf("value of b is %d\n",b);//%d for integer
     printf("value of f is %f\n",f);// %f for float 
+    printf("value of i is %c",i);// %c for character
+    printf("value of j is ")
 }
