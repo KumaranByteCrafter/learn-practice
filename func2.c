90543c56f8ede0900dcab5dea28952331ca066eb
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-06-09 03:29:20 +0000

    function

---
+++ func2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//declare a function
+void call();
+int main()
+{
+    //function call statement
+    call();
+}
+//define a function to happen
+void call(){
+    printf("i am called\n");
+}
