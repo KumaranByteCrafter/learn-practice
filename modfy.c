8fd9ef5c99ecfa1209c9d1e284c2dd2c415cfb90
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-06-09 07:55:24 +0000

    modifiers

---
+++ modfy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+    float b =  65546.678;
+    short signed x = 65535;
+    double d = 65.0000777777777;
+    printf("a = %d\n",a);
+    printf("a = %i\n",a);
+    printf("octal of a = %o\n",a);
+    printf("hexadecimal of a = %X\n",a);
+    printf("short data x  = %d\n",x);
+    printf("b value is = %E\n",b);
+    printf("d  = %f\n",d);
+}
