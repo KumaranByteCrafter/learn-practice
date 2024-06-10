b3f57ea65c93d6e014c570386b8990372381e6ba
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-06-10 17:28:18 +0000

    nestedif

---
+++ nestedif.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num1;
+    printf("Enter the input : ");
+    scanf("%d",&num1);
+    if(num1>0){
+        if(num1<10)
+            printf("value within 0 to 9\n");
+        else
+            printf("value not within 0 to 9\n");
+    }else
+        printf("value is -ve");
+}
